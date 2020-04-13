12be7d9ad4bf551ab93eaf0d1bfdef8191b7c91b
Kevin Kennis <kevinvkennis@gmail.com>
2020-04-12 18:11:14 -0700

    refactor

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -2,19 +2,25 @@ require 'net/http'
 
 class VolunteersController < ApplicationController
   def get
-    uri = URI(Rails.application.config.VOLUNTEERS_AIRTABLE)
-    uri.query = "view=Grid%20view"
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(uri.path, {"Authorization" => "Bearer #{ENV["AIRTABLE_API_KEY"]}"})
-    response = http.request(request)
-    volunteers_json = JSON.parse(response.body)
+    volunteers_json = fetchVolunteerJson;
 
     respond_to do |format|
         format.json do
             render(json: volunteers_json)
         end
     end
+  end
+
+  private
+
+  def fetchVolunteerJson():
+    uri = URI(ENV[VOLUNTEERS_AIRTABLE_URL])
+    uri.query = "view=Grid%20view"
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.path, {"Authorization" => "Bearer #{ENV["AIRTABLE_API_KEY"]}"})
+    response = http.request(request)
+    return JSON.parse(response.body)
   end
 end
