7998cb15320e6ede875e9259be992d6ebe6aab62
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-17 11:48:44 +0100

    Rename serializer

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -1,8 +1,7 @@
-require 'net/http'
-
-class VolunteersController < ApplicationController
+class VolunteerController < ApplicationController
   def index
     volunteers = fetchVolunteerJson;
+    render json: volunteers, status: :ok, serializer: Volunteer
     json_response(volunteers)
   end
 
