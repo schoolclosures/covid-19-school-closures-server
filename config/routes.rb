48f7271ce4578e8633b6f0f1fc9f6a24859ebae3
chanwkkk <chanwk18@hotmail.com>
2020-04-23 13:32:04 -0600

    add routes of subscription and methods in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
     get 'volunteers', to: 'volunteers#index'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resource :subscriptions, only: [:create, :send, :destroy]
+
 end
