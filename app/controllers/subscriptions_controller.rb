d9e5e0c51a0d43fa28d3f27d75ac0934da954d3d
chanwkkk <chanwk18@hotmail.com>
2020-04-23 13:50:27 -0600

    fix the bug that rails direct /subscriptions to show method

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,20 @@
+class SubscriptionsController < ApplicationController
+
+    def index
+        render plain: "index"
+    end
+
+    def create
+
+    end
+
+    def destroy
+
+    end
+
+    def send
+
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     get 'volunteers', to: 'volunteers#index'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resource :subscriptions, only: [:create, :send, :destroy]
+    get '/subscriptions', to: "subscriptions#index"
+    post '/subscriptions', to: "subscriptions#create"
+    delete '/subscriptions/:id', to: "subscriptions#destroy"
 
 end
