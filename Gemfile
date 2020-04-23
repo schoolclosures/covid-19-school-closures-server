bcb5c0bcf26679141a3ee615259ebec218f1d8e2
chanwkkk <chanwk18@hotmail.com>
2020-04-23 15:22:08 -0600

    add create action in subscription controller

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '>= 2.5.1'
 
 gem 'sqlite3'
 
+gem 'pry'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,20 +1,24 @@
 class SubscriptionsController < ApplicationController
 
     def index
-        render plain: "index"
+        render json: { message: 'Hashes of data will get converted to JSON' }
     end
 
     def create
-
+        subscription = Subscription.create(subscription_params)
+        if subscription.save
+            render json: subscription
+        else
+            render json: {error: "Sorry, please try again."}
+        end
     end
 
-    def destroy
+private
 
-    end
+   def subscription_params
+      params.require(:subscription).permit(:email)
+   end
 
-    def send
-
-    end
 
 
 end
