06025c8089ad1fb1cbd49386216a3a62a2479499
Kevin Kennis <kevinvkennis@gmail.com>
2020-05-10 22:01:18 -0700

    finish logic

--- app/controllers/api/v1/newsletter_controller.rb
+++ app/controllers/api/v1/newsletter_controller.rb
@@ -3,22 +3,19 @@ require 'gibbon'
 module Api
   module V1
     class NewsletterController < ApplicationController
-      Gibbon::Request.api_key = "a6b1f36972f29e40e9fab68f43cea585"
 
       def subscribe
-        # Call Mailchimp API with new e-mail address
-        # Return success if added
         begin 
           submittedEmail = params[:emailAddress]
           email_hash = Digest::MD5.hexdigest(submittedEmail)
-          # gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"])
-          gibbon = Gibbon::Request.new(api_key: "a6b1f36972f29e40e9fab68f43cea585")
+          gibbon = Gibbon::Request.new
+
           gibbon.lists(ENV["MAILCHIMP_LIST_ID"])
             .members(email_hash).upsert(
               body: {email_address: submittedEmail, status: "subscribed"}
             )
 
-          json_response(submittedEmail)
+          json_response({ user: submittedEmail, status: "subscribed" })
         rescue => error
           p "Subscribe error: #{error}"
           render json: {
