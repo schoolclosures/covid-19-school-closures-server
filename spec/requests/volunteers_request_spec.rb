fd7b31d6f584b3ebed513863bb526f3f9adf18ed
chanwkkk <chanwk18@hotmail.com>
2020-05-11 11:30:46 -0600

    add expectations to #index

--- spec/requests/volunteers_request_spec.rb
+++ spec/requests/volunteers_request_spec.rb
@@ -4,13 +4,17 @@ RSpec.describe "Volunteers", type: :request do
 
     describe '#index' do
 
+       it 'responds succesfully' 
+
      
 
     end
 
     describe '#show' do
       
-
+      it 'shows the record when given the correct id' do
+        
+      end 
 
     end
 
