0e6ab86ec6061219f5fc3b8f570d40626a1ae815
chanwkkk <chanwk18@hotmail.com>
2020-05-11 11:26:53 -0600

    add a volunteers request spec file

---
+++ spec/requests/volunteers_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Volunteers", type: :request do
+
+    describe '#index' do
+
+     
+
+    end
+
+    describe '#show' do
+      
+
+
+    end
+
+    describe '#create' do
+    end
+
+
+    describe '#update' do
+    end
+
+    describe '#destroy' do
+
+    end
+
+end
