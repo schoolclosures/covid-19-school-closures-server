24239af890a167c4582e70fa24183df33f4abe01
chanwkkk <chanwk18@hotmail.com>
2020-05-11 18:00:19 -0600

    add test for volunteer#create

--- spec/requests/volunteers_request_spec.rb
+++ spec/requests/volunteers_request_spec.rb
@@ -25,23 +25,50 @@ RSpec.describe Api::V1::VolunteersController, type: :request do
 
     describe '#show' do
 
-        before(:each) { get "http://localhost:3000/api/v1/volunteers/#{volunteer1.id}"}
+        before(:each){ get "http://localhost:3000/api/v1/volunteers/#{volunteer1.id}"}
+        let(:response_json){json_parse(response.body)}
 
         it "responds succesfully" do
           expect(response).to have_http_status(200)
         end
 
-        it " returns the information for the right volunteer" do
+        it "returns the information for the right volunteer" do
          #parse response to format json
-          response_json = json_parse(response.body)
           expect(response_json['id']).to eq(volunteer1.id)
         end
-
-
 
     end
 
     describe '#create' do
+
+        let(:params){
+            {:volunteer =>  {
+                "image_url"=>"new.jpg", 
+                "name"=>"new", 
+                "job_desc"=>"newdescription"
+              }
+            }
+        }
+
+        let(:create_volunteer){post "http://localhost:3000/api/v1/volunteers", :params => params}
+
+        before(:each){ create_volunteer }    
+        let(:response_json){json_parse(response.body)}
+ 
+
+       it "responds successfully" do
+          expect(response).to have_http_status(200)
+       end
+
+       it "returns the newly created volunteer object" do
+          expect(response_json).to include(params[:volunteer])
+       end
+
+
+       it "creates one more volunteer" do
+          expect(Volunteer.count).to eq(1)
+       end
+
     end
 
 
